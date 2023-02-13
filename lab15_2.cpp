3d549d291aa93c9d2dac113f337e6c37ad35ca47
fear <corefea@gmail.com>
2023-02-13 22:41:06 +0700

    comitted

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,46 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double d[][N])
+{
+	for(int i = 0; i < N; i++)
+    {
+		cout << "Row " << i + 1 << ": ";
+		for (int j = 0; j < N; j++)
+		{
+			cin >> d[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double d1[][N], bool d2[][N])
+{
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+        {
+			if ((i > 0 && i <= N - 2) && (j > 0 && j <= N - 2))
+			{
+				if ((d1[i][j] >= d1[i - 1][j]) && (d1[i][j] >= d1[i][j - 1]) && (d1[i][j] >= d1[i][j + 1]) && (d1[i][j] >= d1[i + 1][j]))
+				{
+					d2[i][j] = 1;
+				}
+				else d2[i][j] = 0;
+			}
+			else d2[i][j] = 0;
+		}
+	}
+}
+
+void showMatrix(const bool d[][N])
+{
+	for(int i = 0; i < N; i++)
+    {
+		for (int j = 0; j < N; j++)
+		{
+			cout << d[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
